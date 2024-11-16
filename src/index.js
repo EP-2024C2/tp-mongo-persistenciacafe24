582d0bed3bf05679fbccb10d95d973a3871d9d87
WalterAurelio <aurelio.gareca.94@hotmail.com>
2024-11-16 12:58:40 -0300

    Se soluciona error en configuración en docker-compose.yml. Se terminan de definir controladores y endpoints de Productos.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 require('dotenv').config();
+const routes = require('./routes/index');
 const { connectToDatabase, mongoose } = require('./db/mongo.db');
 
 const app = express();
 app.use(express.json());
 
-
+app.use(routes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
-  await connectToDatabase();
+app.listen(PORT, () => {
+  connectToDatabase();
   console.log(`Aplicación corriendo en el puerto ${PORT}...`);
 });
