4d3eb6fd49c093d233f84c2a35e72ee024d759d4
unknown <sebastiancastro141@gmail.com>
2024-11-18 19:51:42 -0300

    cambios

--- src/routes/fabricante.routes.js
+++ src/routes/fabricante.routes.js
@@ -1,14 +1,16 @@
 const { Router } = require('express');
 
 const { getAllFabricantes,getFabricanteById,createFabricante,updateFabricante,deleteFabricante,getProductoDeFabricante,asociarFabricante} = require('../controllers/fabricante.controller');
+const {validateIdEnModelo} = require('../middleares/genericId.middleware')
+const Fabricante = require('../models/fabricante.model')
 
 const route = Router();
 
 route.get('/', getAllFabricantes);
-route.get('/:id', getFabricanteById);
+route.get('/:id',validateIdEnModelo(Fabricante), getFabricanteById);
 route.post('/', createFabricante);
-route.put('/:id', updateFabricante);
+route.put('/:id',validateIdEnModelo(Fabricante), updateFabricante);
 route.delete('/:id', deleteFabricante);
-route.get('/:id/productos',getProductoDeFabricante);
+route.get('/:id/productos',validateIdEnModelo(Fabricante),getProductoDeFabricante);
 
 module.exports = route;
--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -10,16 +10,18 @@ const {
   asociarComponentes,
   getComponentesDelProducto
 } = require('../controllers/producto.controller');
+const {validateIdEnModelo} = require('../middleares/genericId.middleware')
+const Producto = require('../models/producto.model')
 const route = Router();
 
 route.get('/', getAllProductos);
-route.get('/:id', getProductoById);
+route.get('/:id', validateIdEnModelo(Producto), getProductoById);
 route.post('/', createProducto);
-route.put('/:id', updateProducto);
-route.delete('/:id', deleteProducto);
-route.post('/:id/fabricantes', asociarFabricantes);
-route.get('/:id/fabricantes', getFabricantesDelProducto);
-route.post('/:id/componentes', asociarComponentes)
-route.get('/:id/componentes', getComponentesDelProducto);
+route.put('/:id',validateIdEnModelo(Producto), updateProducto);
+route.delete('/:id',validateIdEnModelo(Producto), deleteProducto);
+route.post('/:id/fabricantes',validateIdEnModelo(Producto), asociarFabricantes);
+route.get('/:id/fabricantes',validateIdEnModelo(Producto), getFabricantesDelProducto);
+route.post('/:id/componentes',validateIdEnModelo(Producto), asociarComponentes)
+route.get('/:id/componentes',validateIdEnModelo(Producto), getComponentesDelProducto);
 
 module.exports = route;
