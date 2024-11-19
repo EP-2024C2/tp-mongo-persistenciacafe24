6b3a5293ad3b7483d4905cdb6076475df3983de3
unknown <sebastiancastro141@gmail.com>
2024-11-18 22:16:31 -0300

    implementacion de middlewares

--- src/routes/fabricante.routes.js
+++ src/routes/fabricante.routes.js
@@ -1,15 +1,17 @@
 const { Router } = require('express');
 
 const { getAllFabricantes,getFabricanteById,createFabricante,updateFabricante,deleteFabricante,getProductoDeFabricante,asociarFabricante} = require('../controllers/fabricante.controller');
-const {validateIdEnModelo} = require('../middleares/genericId.middleware')
+const { fabricantesSchema } = require('../schemas/fabricante.schema');
+const schemaValidator = require('../middlewares/schemaValidator');
+const {validateIdEnModelo} = require('../middlewares/genericId.middleware')
 const Fabricante = require('../models/fabricante.model')
 
 const route = Router();
 
 route.get('/', getAllFabricantes);
 route.get('/:id',validateIdEnModelo(Fabricante), getFabricanteById);
-route.post('/', createFabricante);
-route.put('/:id',validateIdEnModelo(Fabricante), updateFabricante);
+route.post('/',schemaValidator(fabricantesSchema), createFabricante);
+route.put('/:id',schemaValidator(fabricantesSchema),validateIdEnModelo(Fabricante), updateFabricante);
 route.delete('/:id', deleteFabricante);
 route.get('/:id/productos',validateIdEnModelo(Fabricante),getProductoDeFabricante);
 
--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -10,18 +10,21 @@ const {
   asociarComponentes,
   getComponentesDelProducto
 } = require('../controllers/producto.controller');
-const {validateIdEnModelo} = require('../middleares/genericId.middleware')
+const { productosSchema } = require('../schemas/productos.schema');
+const schemaValidator = require('../middlewares/schemaValidator');
+const arraySchema = require('../schemas/array.schema');
+const {validateIdEnModelo} = require('../middlewares/genericId.middleware')
 const Producto = require('../models/producto.model')
 const route = Router();
 
 route.get('/', getAllProductos);
 route.get('/:id', validateIdEnModelo(Producto), getProductoById);
-route.post('/', createProducto);
-route.put('/:id',validateIdEnModelo(Producto), updateProducto);
+route.post('/',schemaValidator(productosSchema), createProducto);
+route.put('/:id',schemaValidator(productosSchema),validateIdEnModelo(Producto), updateProducto);
 route.delete('/:id',validateIdEnModelo(Producto), deleteProducto);
-route.post('/:id/fabricantes',validateIdEnModelo(Producto), asociarFabricantes);
+route.post('/:id/fabricantes',schemaValidator(arraySchema('fabricantes')),validateIdEnModelo(Producto), asociarFabricantes);
 route.get('/:id/fabricantes',validateIdEnModelo(Producto), getFabricantesDelProducto);
-route.post('/:id/componentes',validateIdEnModelo(Producto), asociarComponentes)
+route.post('/:id/componentes',schemaValidator(arraySchema('componentes')),validateIdEnModelo(Producto), asociarComponentes)
 route.get('/:id/componentes',validateIdEnModelo(Producto), getComponentesDelProducto);
 
 module.exports = route;
